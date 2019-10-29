d67ca102b5c3244191059a942900a7cd63ca65e2
AustinLynes <austinlynes94@gmail.com>
2019-10-29 10:04:40 -0700

    MVP -- Completed

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ server.get('/api/users/:id', (req, res) => {
     const id = req.params.id
     user_db.findById(id)
         .then(user => {
+            if (!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }
             res.status(200).json(user)
         })
         .catch(() => {
@@ -36,24 +39,67 @@ server.get('/api/users/:id', (req, res) => {
 
 server.post('/api/users/', (req, res) => {
     const user = req.body
-    const users = user_db.find()
-    console.log(req.body)
-    if(user.name !== undefined || user.bio !== undefined){
-        user_db.insert(user)
-        .then(_user =>{
-            res.status(201).json({message:'user created successfully!'})
+    user_db.insert(user)
+        .then(_user => {
+            if (!_user.name || !use.bio) {
+                res.status(400).json({
+                    errorMessage: "Please provide name and bio for the user."
+                })
+            }
+            res.status(201).json({ message: 'user created successfully!' })
         })
-        .catch(()=>{
-            res.status(500).json({errorMessage:'some information was not found...'})
+        .catch(() => {
+            res.status(500).json({
+                error: "The user information could not be retrieved."
+            })
         })
-    }else{
-        res.status(400).json({
-            errorMessage:'information is not unidentified'
-        })
-    }
-   
+
+
 })
 
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    console.log(id)
+    user_db.findById(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist."
+                })
+            } else {
+                user_db.remove(id)
+                    .then(() => {
+                        res.status(200).json({
+                            message: `${user.name} has been deleted from the database`
+                        })
+                    })
+                    .catch(() => {
+                        res.status(500).json({
+                            error: "The user could not be removed"
+                        })
+                    })
+            }
+        })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const newInfo = req.body
+    user_db.findById(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json(
+                    { message: "The user with the specified ID does not exist." }
+                )
+            }
+        })
+        user_db.update(id, {...newInfo})
+        .then(user=>{
+            res
+            .status(200)
+            .json(user)
+        })
+})
 
 server.listen(port, () => {
     console.log(`server is listening on port ${port}`)
